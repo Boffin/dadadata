af93c28e6c8ee7496ed0a62d5e01c2145010945a
b0ff1n <eugene@tshts.pw>
2014-12-26 15:25:08 +0200

    datomic dependency

--- project.clj
+++ project.clj
@@ -1,6 +1,12 @@
 (defproject dadadata "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :repositories [["my.datomic.com" {:url "https://my.datomic.com/repo"
+                                    :username [:env/my_datomic_username]
+                                    :password [:env/my_datomic_password]}]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.stuartsierra/component "0.2.2"]
+                 [com.datomic/datomic-pro "0.9.5067"]]
+  :main dadadata.core)
