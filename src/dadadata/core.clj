1d16e17cf46202ce473305de08b506f16843fa36
b0ff1n <eugene@tshts.pw>
2014-12-27 12:01:56 +0200

    very basic amqp producer and consumer

--- src/dadadata/core.clj
+++ src/dadadata/core.clj
@@ -17,15 +17,36 @@
           (lb/publish channel exchange routing-key message))
         (log/error "channel is closed")))))
 
+(defn make-consumer [connection queue]
+  (let [channel (lch/open connection)]
+    (fn []
+      (if (lch/open? channel)
+        (do
+          (lb/get channel queue))
+        (log/error "channel is closed")))))
+
+(defn publish [amqp-config message]
+  (let [connection (rmq/connect)
+        publisher (make-publisher connection (amqp-config :exchange) (amqp-config :routing-key))]
+    (publisher message)))
+
+(defn consume [queue]
+  (let [connection (rmq/connect)
+        consumer (make-consumer connection queue)]
+    (consumer)))
+
 (defn -main
   ([] (-main "etc/dadadata.edn"))
   ([config-file]
-    (def config (edn/read-string (slurp config-file)))
-    (let [connection (rmq/connect)
-          publisher (make-publisher connection ((config :ampq) :exchange) ((config :ampq) :routing-key))]
-      (println (publisher "dflkdf"))
-      (publisher "abcd")
-      (println (class publisher)))))
+   (def config (edn/read-string (slurp config-file)))
+   (publish (config :amqp) "aaaaaaaaaaaa")
+   (consume ((config :amqp) :queue))))
+
+(-main)
+
+
+
+
 ;(defn -main [config-file]
 ;  (let [config (edn/read-string (slurp config-file))
 ;        system (component/start (new-system config))]
